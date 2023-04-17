650ded097e982a62448fade4b3d38b4e150c43d2
KarimKarimov1902 <karim@adm-center.ru>
2023-04-17 13:31:34 +0300

    init

--- test.php
+++ test.php
@@ -1,6 +1,6 @@
 <?
 require($_SERVER["DOCUMENT_ROOT"]."/bitrix/header.php");
-$APPLICATION->SetTitle("Новая страница");
+$APPLICATION->SetTitle("Новая страница"); 
 ?><?$APPLICATION->IncludeComponent(
 	"bitrix:iblock.element.add.form", 
 	".default", 
